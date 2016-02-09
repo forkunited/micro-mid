5486d51beecff2e90c2656091c05bb943c166282
Bill McDowell <forkunited@gmail.com>
2016-02-09 15:51:04 -0500

    Fixed bug

--- src/main/java/edu/psu/ist/acs/micro/mid/scratch/ConstructMID4NewsDocumentSet.java
+++ src/main/java/edu/psu/ist/acs/micro/mid/scratch/ConstructMID4NewsDocumentSet.java
@@ -125,8 +125,8 @@ public class ConstructMID4NewsDocumentSet {
 				String[] lineTokens = line.split("\\s+");
 				
 				if (lineTokens.length > 0 && 
-						(lineTokens[0].toLowerCase().matches("x+")
-								|| lineTokens[0].toLowerCase().matches("xxx+.+"))) {
+						(lineTokens[0].toLowerCase().matches("xx+.*")
+								|| lineTokens[0].toLowerCase().matches("xx+.*"))) {
 					if (!processDocument(file.getName(), file.getName() + "." + id, documentStr.toString().trim(), ternaryClass)) {
 						if (prevDocument != null) {
 							System.out.println("--------------------------------\n\n\nPrior to failure, the most recent processed document was: " + prevDocument);
