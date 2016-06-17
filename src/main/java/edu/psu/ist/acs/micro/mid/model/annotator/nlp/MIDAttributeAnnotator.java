f2f633bf06bff608592a10fd45b4f9fe8bb5a1f0
Bill McDowell <forkunited@gmail.com>
2016-06-17 17:36:35 -0400

    ...

--- src/main/java/edu/psu/ist/acs/micro/mid/model/annotator/nlp/MIDAttributeAnnotator.java
+++ src/main/java/edu/psu/ist/acs/micro/mid/model/annotator/nlp/MIDAttributeAnnotator.java
@@ -1,50 +1,31 @@
 package edu.psu.ist.acs.micro.mid.model.annotator.nlp;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
-
-
-
-
-
-import edu.cmu.ml.rtw.generic.data.StoredItemSet;
 import edu.cmu.ml.rtw.generic.data.annotation.AnnotationType;
-import edu.cmu.ml.rtw.generic.data.annotation.DataSet;
 import edu.cmu.ml.rtw.generic.data.annotation.Datum.Tools;
 import edu.cmu.ml.rtw.generic.data.annotation.DatumContext;
 import edu.cmu.ml.rtw.generic.data.annotation.nlp.AnnotationTypeNLP;
 import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLP;
 import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLPDatum;
-import edu.cmu.ml.rtw.generic.data.annotation.nlp.TokenSpan;
-import edu.cmu.ml.rtw.generic.data.annotation.nlp.TokenSpansDatum;
-import edu.cmu.ml.rtw.generic.data.store.StoreReference;
-import edu.cmu.ml.rtw.generic.model.annotator.nlp.AnnotatorTokenSpan;
+import edu.cmu.ml.rtw.generic.model.annotator.nlp.AnnotatorDocument;
 import edu.cmu.ml.rtw.generic.parse.Obj;
-import edu.cmu.ml.rtw.generic.task.classify.MethodClassification;
 import edu.cmu.ml.rtw.generic.task.classify.MethodClassificationSupervisedModel;
 import edu.cmu.ml.rtw.generic.util.OutputWriter;
 import edu.cmu.ml.rtw.generic.util.Pair;
-import edu.cmu.ml.rtw.generic.util.Triple;
 import edu.cmu.ml.rtw.generic.util.WeightedStringList;
-import edu.psu.ist.acs.micro.event.data.EventDataTools;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.AnnotationTypeNLPEvent;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.event.EventMention;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.event.EventMention.TimeMLAspect;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.event.EventMention.TimeMLClass;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.event.EventMention.TimeMLPolarity;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.event.EventMention.TimeMLTense;
-import edu.psu.ist.acs.micro.event.data.annotation.nlp.event.EventMentionDatum;
-import edu.psu.ist.acs.micro.event.util.EventProperties;
 import edu.psu.ist.acs.micro.mid.data.MIDDataTools;
+import edu.psu.ist.acs.micro.mid.data.annotation.MIDIncident;
 import edu.psu.ist.acs.micro.mid.data.annotation.nlp.AnnotationTypeNLPMID;
 import edu.psu.ist.acs.micro.mid.util.MIDProperties;
 
 
-public class MIDAttributeAnnotator { /*implements AnnotatorTokenSpan<WeightedStringList> {
+public class MIDAttributeAnnotator implements AnnotatorDocument<WeightedStringList> {
 	private static final AnnotationType<?>[] REQUIRED_ANNOTATIONS = new AnnotationType<?>[] {
 		AnnotationTypeNLP.TOKEN,
 		AnnotationTypeNLP.POS,
@@ -55,44 +36,64 @@ public class MIDAttributeAnnotator { /*implements AnnotatorTokenSpan<WeightedStr
 	};
 	
 	public static final File DEFAULT_MID_ATTRIBUTE_MODEL_FILE = new File("");
-	public static final String DEFAULT_MID_RELEVANCE_MODEL_PARSE_PATH = "";
+	public static final String DEFAULT_MID_ATTRIBUTE_MODEL_PARSE_PATH_PREFIX = "";
+	public static final String DEFAULT_MID_ATTRIBUTE_MODEL_PARSE_PATH_SUFFIX = "";
 	
 	private MIDDataTools dataTools;
-	private Tools<DocumentNLPDatum<Boolean>, Boolean> midRelevanceDatumTools;
+	private Tools<DocumentNLPDatum<Boolean>, Boolean> midAttributeDatumTools;
 	
-	private MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean> midRelevanceClassifier;
+	private Map<String, MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean>> midAttributeClassifiers;
 
-	public MIDRelevanceAnnotator() {
-		this(DEFAULT_MID_RELEVANCE_MODEL_FILE);
+	public MIDAttributeAnnotator() {
+		this(DEFAULT_MID_ATTRIBUTE_MODEL_FILE);
 	}
 
-	public MIDRelevanceAnnotator(File midRelevanceModelFile) {
-		this(midRelevanceModelFile, DEFAULT_MID_RELEVANCE_MODEL_PARSE_PATH);
+	public MIDAttributeAnnotator(File midAttrModelFile) {
+		this(midAttrModelFile, DEFAULT_MID_ATTRIBUTE_MODEL_PARSE_PATH_PREFIX, DEFAULT_MID_ATTRIBUTE_MODEL_PARSE_PATH_SUFFIX);
 	}
 	
-	public MIDRelevanceAnnotator(File midRelevanceModelFile, String midRelevanceModelParsePath) {
+	public MIDAttributeAnnotator(File midAttrModelFile, String midAttrModelParsePathPrefix, String midAttrModelParsePathSuffix) {
 		this.dataTools = new MIDDataTools(new OutputWriter(), new MIDProperties());
-		this.midRelevanceDatumTools = DocumentNLPDatum.getBooleanTools(this.dataTools);
+		this.midAttributeDatumTools = DocumentNLPDatum.getBooleanTools(this.dataTools);
 		
-		if (!deserialize(midRelevanceModelFile,
-						 midRelevanceModelParsePath))
+		if (!deserialize(midAttrModelFile,
+						 midAttrModelParsePathPrefix,
+						 midAttrModelParsePathSuffix))
 			throw new IllegalArgumentException();
 	}
 	
-	public boolean deserialize(File midRelevanceModelFile, String midRelevanceModelParsePath) {		
-		this.midRelevanceClassifier = (MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean>)
-				DatumContext.run(this.midRelevanceDatumTools, midRelevanceModelFile).getMatchClassifyMethod(Obj.curlyBracedValue(midRelevanceModelParsePath));
+	public boolean deserialize(File midAttrModelFile, String midAttrModelParsePathPrefix, String midAttrModelParsePathSuffix) {		
+		this.midAttributeClassifiers = new TreeMap<>();
+		
+		Set<String> attrs = new HashSet<String>();
+		for (MIDIncident.HostilityLevel hostLevel : MIDIncident.HostilityLevel.values()) {
+			attrs.add(hostLevel.toString());
+		}
+		
+		for (MIDIncident.Action action : MIDIncident.Action.values()) {
+			attrs.add(action.toString());
+		}
+		
+		DatumContext<DocumentNLPDatum<Boolean>, Boolean> ctx = DatumContext.run(this.midAttributeDatumTools, midAttrModelFile);
+		
+		for (String attr : attrs) {
+			String parsePath = midAttrModelParsePathPrefix + attr + midAttrModelParsePathSuffix;
+			this.midAttributeClassifiers.put(attr, 
+					(MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean>)
+						ctx.getMatchClassifyMethod(Obj.curlyBracedValue(parsePath)));
+		}
+		
 		return true;
 	}
 
 	@Override
 	public String getName() {
-		return "psu_mid_rel-0.0.1";
+		return "psu_mid_attr-0.0.1";
 	}
 
 	@Override
-	public AnnotationType<Boolean> produces() {
-		return AnnotationTypeNLPMID.MID_CLASSIFIER_RELEVANCE_CLASS;
+	public AnnotationType<WeightedStringList> produces() {
+		return AnnotationTypeNLPMID.MID_ATTRIBUTE;
 	}
 
 	@Override
@@ -102,15 +103,26 @@ public class MIDAttributeAnnotator { /*implements AnnotatorTokenSpan<WeightedStr
 
 	@Override
 	public boolean measuresConfidence() {
-		return true;
+		return false;
 	}
 
-	private DocumentNLPDatum<Boolean> constructMIDRelevanceDatum(DocumentNLP document) {
+	private DocumentNLPDatum<Boolean> constructMIDAttributeDatum(DocumentNLP document) {
 		return new DocumentNLPDatum<Boolean>(this.dataTools.getIncrementId(), document, null);
 	}
 	
 	@Override
-	public Pair<Boolean, Double> annotate(DocumentNLP document) {
-		return this.midRelevanceClassifier.classifyWithScore(constructMIDRelevanceDatum(document));
-	}*/
+	public Pair<WeightedStringList, Double> annotate(DocumentNLP document) {
+		DocumentNLPDatum<Boolean> datum = constructMIDAttributeDatum(document);
+
+		Map<String, Double> scores = new TreeMap<>();
+		for (Entry<String, MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean>> entry : this.midAttributeClassifiers.entrySet()) {
+			Pair<Boolean, Double> c = entry.getValue().classifyWithScore(datum);
+			if (c.getFirst()) {
+				double score = c.getSecond();
+				scores.put(entry.getKey(), score);
+			}
+		}
+		WeightedStringList anno = new WeightedStringList(scores);
+		return new Pair<WeightedStringList, Double>(anno, null);
+	}
 }
