f297e617a1d742544a1b606568db6b1c34a22241
Bill McDowell <forkunited@gmail.com>
2016-06-16 21:04:08 -0400

    ...

--- src/main/java/edu/psu/ist/acs/micro/mid/data/annotation/nlp/AnnotationTypeNLPMID.java
+++ src/main/java/edu/psu/ist/acs/micro/mid/data/annotation/nlp/AnnotationTypeNLPMID.java
@@ -3,6 +3,7 @@ package edu.psu.ist.acs.micro.mid.data.annotation.nlp;
 import edu.cmu.ml.rtw.generic.data.annotation.nlp.AnnotationTypeNLP;
 import edu.cmu.ml.rtw.generic.data.annotation.nlp.AnnotationTypeNLP.Target;
 import edu.psu.ist.acs.micro.mid.data.annotation.MIDDispute;
+import edu.psu.ist.acs.micro.mid.data.annotation.MIDIncident.HostilityLevel;
 
 /**
  * 
@@ -35,5 +36,29 @@ public class AnnotationTypeNLPMID {
 	public static final AnnotationTypeNLP<String> ARTICLE_BYLINE = new AnnotationTypeNLP<String>("article-byline", String.class, Target.DOCUMENT);
 	public static final AnnotationTypeNLP<String> ARTICLE_DATELINE = new AnnotationTypeNLP<String>("article-dateline", String.class, Target.DOCUMENT);
 
-
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_NO_MILITARIZED_ACTION = new AnnotationTypeNLP<Boolean>("mid-attr-no-militarized-action", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_THREAT_TO_USE_FORCE = new AnnotationTypeNLP<Boolean>("mid-attr-threat-to-use-force", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_THREAT_TO_BLOCKADE = new AnnotationTypeNLP<Boolean>("mid-attr-threat-to-blockade", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_THREAT_TO_OCCUPY_TERRITORY = new AnnotationTypeNLP<Boolean>("mid-attr-threat-to-occupy-territory", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_THREAT_TO_DECLARE_WAR = new AnnotationTypeNLP<Boolean>("mid-attr-threat-to-declare-war", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_THREAT_TO_USE_CBR_WEAPONS = new AnnotationTypeNLP<Boolean>("mid-attr-threat-to-use-cbr-weapons", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_THREAT_TO_JOIN_WAR = new AnnotationTypeNLP<Boolean>("mid-attr-threat-to-join-war", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_DISPLAY_OF_FORCE = new AnnotationTypeNLP<Boolean>("mid-attr-display-of-force", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_SHOW_OF_FORCE = new AnnotationTypeNLP<Boolean>("mid-attr-show-of-force", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_ALERT = new AnnotationTypeNLP<Boolean>("mid-attr-alert", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_NUCLEAR_ALERT = new AnnotationTypeNLP<Boolean>("mid-attr-nuclear-alert", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_MOBILIZATION = new AnnotationTypeNLP<Boolean>("mid-attr-mobilization", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_FORTIFY_BORDER = new AnnotationTypeNLP<Boolean>("mid-attr-fortify-border", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_BORDER_VIOLATION = new AnnotationTypeNLP<Boolean>("mid-attr-border-violation", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_USE_OF_FORCE = new AnnotationTypeNLP<Boolean>("mid-attr-use-of-force", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_BLOCKADE = new AnnotationTypeNLP<Boolean>("mid-attr-blockade", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_OCCUPATION_OF_TERRITORY = new AnnotationTypeNLP<Boolean>("mid-attr-occupation-of-territory", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_SEIZURE = new AnnotationTypeNLP<Boolean>("mid-attr-seizure", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_ATTACK = new AnnotationTypeNLP<Boolean>("mid-attr-attack", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_CLASH = new AnnotationTypeNLP<Boolean>("mid-attr-clash", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_DECLARATION_OF_WAR = new AnnotationTypeNLP<Boolean>("mid-attr-declaration-of-war", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_USE_OF_CBR_WEAPONS = new AnnotationTypeNLP<Boolean>("mid-attr-use-of-cbr-weapons", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_WAR = new AnnotationTypeNLP<Boolean>("mid-attr-war", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_BEGIN_INTERSTATE_WAR = new AnnotationTypeNLP<Boolean>("mid-attr-begin-interstate-war", Boolean.class, Target.DOCUMENT);
+	public static final AnnotationTypeNLP<Boolean> MID_ATTRIBUTE_JOIN_INTERSTATE_WAR = new AnnotationTypeNLP<Boolean>("mid-attr-join-interstate-war", Boolean.class, Target.DOCUMENT);
 }
---
+++ src/main/java/edu/psu/ist/acs/micro/mid/model/annotator/nlp/MIDRelevanceAnnotator.java
@@ -0,0 +1,85 @@
+package edu.psu.ist.acs.micro.mid.model.annotator.nlp;
+
+import java.io.File;
+
+import edu.cmu.ml.rtw.generic.data.annotation.AnnotationType;
+import edu.cmu.ml.rtw.generic.data.annotation.Datum.Tools;
+import edu.cmu.ml.rtw.generic.data.annotation.DatumContext;
+import edu.cmu.ml.rtw.generic.data.annotation.nlp.AnnotationTypeNLP;
+import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLP;
+import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLPDatum;
+import edu.cmu.ml.rtw.generic.model.annotator.nlp.AnnotatorDocument;
+import edu.cmu.ml.rtw.generic.parse.Obj;
+import edu.cmu.ml.rtw.generic.task.classify.MethodClassificationSupervisedModel;
+import edu.cmu.ml.rtw.generic.util.OutputWriter;
+import edu.cmu.ml.rtw.generic.util.Pair;
+import edu.psu.ist.acs.micro.mid.data.MIDDataTools;
+import edu.psu.ist.acs.micro.mid.data.annotation.nlp.AnnotationTypeNLPMID;
+import edu.psu.ist.acs.micro.mid.util.MIDProperties;
+
+public class MIDRelevanceAnnotator implements AnnotatorDocument<Boolean> {
+	private static final AnnotationType<?>[] REQUIRED_ANNOTATIONS = new AnnotationType<?>[] {
+		AnnotationTypeNLP.TOKEN,
+		AnnotationTypeNLP.POS
+	};
+	
+	public static final File DEFAULT_MID_RELEVANCE_MODEL_FILE = new File("");
+	public static final String DEFAULT_MID_RELEVANCE_MODEL_PARSE_PATH = "";
+	
+	private MIDDataTools dataTools;
+	private Tools<DocumentNLPDatum<Boolean>, Boolean> midRelevanceDatumTools;
+	
+	private MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean> midRelevanceClassifier;
+
+	public MIDRelevanceAnnotator() {
+		this(DEFAULT_MID_RELEVANCE_MODEL_FILE);
+	}
+
+	public MIDRelevanceAnnotator(File midRelevanceModelFile) {
+		this(midRelevanceModelFile, DEFAULT_MID_RELEVANCE_MODEL_PARSE_PATH);
+	}
+	
+	public MIDRelevanceAnnotator(File midRelevanceModelFile, String midRelevanceModelParsePath) {
+		this.dataTools = new MIDDataTools(new OutputWriter(), new MIDProperties());
+		this.midRelevanceDatumTools = DocumentNLPDatum.getBooleanTools(this.dataTools);
+		
+		if (!deserialize(midRelevanceModelFile,
+						 midRelevanceModelParsePath))
+			throw new IllegalArgumentException();
+	}
+	
+	public boolean deserialize(File midRelevanceModelFile, String midRelevanceModelParsePath) {		
+		this.midRelevanceClassifier = (MethodClassificationSupervisedModel<DocumentNLPDatum<Boolean>, Boolean>)
+				DatumContext.run(this.midRelevanceDatumTools, midRelevanceModelFile).getMatchClassifyMethod(Obj.curlyBracedValue(midRelevanceModelParsePath));
+		return true;
+	}
+
+	@Override
+	public String getName() {
+		return "psu_midrel-0.0.1";
+	}
+
+	@Override
+	public AnnotationType<Boolean> produces() {
+		return AnnotationTypeNLPMID.MID_CLASSIFIER_RELEVANCE_CLASS;
+	}
+
+	@Override
+	public AnnotationType<?>[] requires() {
+		return REQUIRED_ANNOTATIONS;
+	}
+
+	@Override
+	public boolean measuresConfidence() {
+		return true;
+	}
+
+	private DocumentNLPDatum<Boolean> constructMIDRelevanceDatum(DocumentNLP document) {
+		return new DocumentNLPDatum<Boolean>(this.dataTools.getIncrementId(), document, null);
+	}
+	
+	@Override
+	public Pair<Boolean, Double> annotate(DocumentNLP document) {
+		return this.midRelevanceClassifier.classifyWithScore(constructMIDRelevanceDatum(document));
+	}
+}
